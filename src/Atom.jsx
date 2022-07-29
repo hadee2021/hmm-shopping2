46468f6828ca1516091dac379d545a0ec62735ca
hadee2021 <tpghks9920@naver.com>
2022-07-29 10:34:28 +0900

    FIX:atom

--- src/Atom.jsx
+++ src/Atom.jsx
@@ -1,16 +1,19 @@
 import { atom } from 'recoil'
+import { recoilPersist } from 'recoil-persist'
+
+const { persistAtom } = recoilPersist({
+  key: 'auth',
+  storage: localStorage,
+})
 
 export const authenticateAtom = atom({
   key: 'auth',
   default: false,
+  effects_UNSTABLE: [persistAtom],
 })
+
 
 export const cartAtom = atom({
   key: 'cart',
   default: [],
-})
-
-export const loadingAtiom = atom({
-  key: 'loading',
-  default: true,
 })
